63038e5c1bab5f75ff81a8924ba79d178fe44f0f
JariRintaaho <67269415+JariRintaaho@users.noreply.github.com>
2024-09-25 10:36:40 +0300

    Update random_person_generator.py

--- random_person_generator.py
+++ random_person_generator.py
@@ -1,6 +1,23 @@
 from name_generator import name_generator
 from address_generator import address_generator
 
+import csv
+
+# stores data to csv file
+def dict_to_csv(dictionary, filename):
+    keys = list(dictionary.keys())
+    values = list(dictionary.values())
+
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(keys)
+        writer.writerows(zip(*values))
+
+first_names = []
+family_names = []
+street_addresses = []
+postal_codes = []
+post_offices = []
 
 for i in range(10):
 
@@ -10,11 +27,18 @@ for i in range(10):
     address = address_generator()
     address.generate_address()
 
-    personal_details = {"first_name": ' '.join(name.first_names), 
-                        "family_name": name.family_name, 
-                        "street_address" : address.street_address,
-                        "postal_code" : address.postal_code,
-                        "post_office" : address.post_office
-                        }
+    first_names.append(' '.join(name.first_names))
+    family_names.append(name.family_name)
+    street_addresses.append(address.street_address)
+    postal_codes.append(address.postal_code)
+    post_offices.append(address.post_office)
 
-    print(personal_details)
+person_data = {
+    "first_name":first_names,
+    "family_name":family_names,
+    "street_address":street_addresses,
+    "postal_code":postal_codes,
+    "post_office":post_offices
+}
+
+dict_to_csv(person_data, "persons.csv")
